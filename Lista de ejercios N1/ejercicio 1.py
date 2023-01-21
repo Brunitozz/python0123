bbc3f6e06c17e3977fc97ab085174e80409f0ff4
Brunitozz <122190257+Brunitozz@users.noreply.github.com>
2023-01-20 19:39:55 -0500

    primera

---
+++ Lista de ejercios N1/ejercicio 1.py
@@ -0,0 +1,3 @@
+lado=int(input('ingrese la medida del lado del cuadrado: '))
+for l in range(lado):
+    print('*  '*(lado))
---
+++ Lista de ejercios N1/ejercicio 3.py
@@ -0,0 +1,3 @@
+lado=int(input('ingrese la medida del lado del cuadrado:', end=''))
+for l in range(lado+1):
+    print('*'*(lado+1))
