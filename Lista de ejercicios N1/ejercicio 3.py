b3a43b59d7c189a999abadccccfe83be4f65c462
Brunitozz <122190257+Brunitozz@users.noreply.github.com>
2023-01-20 22:51:02 -0500

    Update ejercicio 3.py

--- Lista de ejercicios N1/ejercicio 3.py
+++ Lista de ejercicios N1/ejercicio 3.py
@@ -1,3 +1,6 @@
-lado=int(input('ingrese la medida del lado del cuadrado:', end=''))
-for l in range(lado+1):
-    print('*'*(lado+1))
+print('lista de 15 personas con sus respectivas edades: ')
+personas=[["Ezequiel",4],["Claudia",19],["Norma",6],["Liliana",7],["Víctor",12],['Liliana',10],["Mirta",18],["Ricardo",14],["Carlos",8],["Juana",3],["Nicolás",6],["Silvia",18],["Claudio",20],["Sergio",5],["Martín",7]]
+print(personas)
+for i in range(14):
+    if personas[i][1]>=18:
+        print(f'{personas[i][0]} es mayor de edad')
